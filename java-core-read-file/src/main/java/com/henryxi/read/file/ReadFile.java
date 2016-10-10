d0b8aeb08031746bd7ba5bbf5e765bdb1121e878
yong <xxy668@foxmail.com>
2016-10-10 15:13:52 +0800

    read content of txt file line by line

---
+++ java-core-read-file/src/main/java/com/henryxi/read/file/ReadFile.java
@@ -0,0 +1,19 @@
+package com.henryxi.read.file;
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+
+public class ReadFile {
+    public static void main(String[] args) throws Exception {
+        File file = new File("D:\\testData\\www.csdn.net.sql");
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
+    }
+}
