0cc3786cb1c5515d9bd5069a73232873c287147a
yong <xxy668@foxmail.com>
2017-06-03 10:07:24 +0800

    use jdk8 stream convert list to map

--- java-core-collection/src/main/java/com/henryxi/java/core/collection/util/User.java
+++ java-core-collection/src/main/java/com/henryxi/java/core/collection/util/User.java
@@ -4,6 +4,11 @@ public class User {
     private int id;
     private String name;
 
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public int getId() {
         return id;
     }
@@ -18,5 +23,13 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
