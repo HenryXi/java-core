cc4bce202d73c349b6af9fd1a91a5138cc8e1811
yong <xixiaoyong@xiaomi.com>
2018-01-08 13:42:52 +0800

    sample code

---
+++ java-core-util/src/main/java/com/henryxi/core/util/collection/hashmap/HashMapClient.java
@@ -0,0 +1,9 @@
+package com.henryxi.core.util.collection.hashmap;
+
+public class HashMapClient {
+    public static void main(String[] args) {
+        int hash = 17;
+        int length = 16;
+        System.out.println((length - 1) & hash);
+    }
+}
---
+++ java-core-util/src/main/java/com/henryxi/core/util/collection/list/IndexOfClient.java
@@ -0,0 +1,11 @@
+package com.henryxi.core.util.collection.list;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class IndexOfClient {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("a", "b", "c", "d");
+        System.out.println(list.indexOf("a"));
+    }
+}
