c40aa2fdb3a516641fd5a7d6276508f06f2270d5
yong <xixiaoyong@xiaomi.com>
2018-11-13 16:11:54 +0800

    uuid test

---
+++ java-core-util/src/main/java/com/henryxi/core/util/uuid/UUIDClient.java
@@ -0,0 +1,9 @@
+package com.henryxi.core.util.uuid;
+
+import java.util.UUID;
+
+public class UUIDClient {
+    public static void main(String[] args) {
+        System.out.println(UUID.randomUUID().toString());
+    }
+}
