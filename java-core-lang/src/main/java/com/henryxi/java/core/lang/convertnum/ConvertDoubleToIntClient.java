665acf53b5af52e3cd89e147fd55beb6a86608d5
yong <xixiaoyong@xiaomi.com>
2018-12-05 15:16:32 +0800

    double to int

---
+++ java-core-lang/src/main/java/com/henryxi/java/core/lang/convertnum/ConvertDoubleToIntClient.java
@@ -0,0 +1,8 @@
+package com.henryxi.java.core.lang.convertnum;
+
+public class ConvertDoubleToIntClient {
+    public static void main(String[] args) {
+        double a = 2.6d;
+        System.out.println((int) a);
+    }
+}
