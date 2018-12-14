14f57ed78ce4ba311611976e60fd8dcea6b389ee
yong <xixiaoyong@xiaomi.com>
2018-12-14 21:04:00 +0800

    try to understand volatile

--- java-core-misc/src/main/java/com/henryxi/core/misc/volatile_/VolatileClient.java
+++ java-core-misc/src/main/java/com/henryxi/core/misc/volatile_/VolatileClient.java
@@ -1,7 +1,33 @@
 package com.henryxi.core.misc.volatile_;
 
 public class VolatileClient {
-    public static void main(String[] args) {
+    public static int[] arr = new int[20];
+    
 
+    public static void main(String[] args) throws Exception {
+        new Thread(new Thread() {
+            @Override
+            public void run() {
+                //Thread A
+                do {
+                    if (arr[19] == 0) {
+                        System.out.println("Thread A print.");
+                        arr[19] = 2;
+                    }
+                } while (true);
+            }
+        }).start();
+        new Thread(new Thread() {
+            @Override
+            public void run() {
+                //Thread B
+                do {
+                    if (arr[19] == 2) {
+                        System.out.println("Thread B print.");
+                        arr[19] = 0;
+                    }
+                } while (true);
+            }
+        }).start();
     }
 }
