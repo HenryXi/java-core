a691539a3a351ecc97ab53d1e489c9e14ead2b62
yong <xixiaoyong@xiaomi.com>
2018-12-21 13:48:15 +0800

    time different

--- java-core-time/src/main/java/com/henryxi/core/time/TimeDifferentClient.java
+++ java-core-time/src/main/java/com/henryxi/core/time/TimeDifferentClient.java
@@ -1,14 +1,25 @@
 package com.henryxi.core.time;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.temporal.TemporalUnit;
+import java.util.Date;
+import java.util.Locale;
 
 public class TimeDifferentClient {
-    public static void main(String[] args) throws InterruptedException {
-        Instant firstTime = Instant.ofEpochMilli(System.currentTimeMillis());
-        Thread.sleep(10 * 1000);
-        Instant secondTime = Instant.ofEpochMilli(System.currentTimeMillis());
-        Duration timeElapsed = Duration.between(firstTime, secondTime);
-        System.out.println("duration second:" + timeElapsed.getSeconds());
+    public static void main(String[] args) throws InterruptedException, ParseException {
+        String dateTime1 = "Mon 17:00";
+        String dateTime2 = "Tue 5:00";
+        SimpleDateFormat formatterUS = new SimpleDateFormat("EEE HH:mm", Locale.US);
+        Date date1 = formatterUS.parse(dateTime1);
+        Date date2 = formatterUS.parse(dateTime2);
+        LocalDateTime localDateTime1 = LocalDateTime.ofInstant(date1.toInstant(), ZoneId.systemDefault());
+        LocalDateTime localDateTime2 = LocalDateTime.ofInstant(date2.toInstant(), ZoneId.systemDefault());
+        Duration timeElapsed = Duration.between(localDateTime1, localDateTime2);
+        System.out.println("duration second:" + timeElapsed.getUnits());
     }
 }
