3e6c74c1f7cda170426bdc1ad700a5339be5052f
yong <xixiaoyong@xiaomi.com>
2019-06-22 09:40:00 +0800

    Java get local address example

---
+++ java-core-net/src/main/java/com/henryxi/net/GetLocalAddressClient.java
@@ -0,0 +1,11 @@
+package com.henryxi.net;
+
+import java.net.InetAddress;
+
+public class GetLocalAddressClient {
+    public static void main(String args[]) throws Exception {
+        InetAddress inetAddress = InetAddress.getLocalHost();
+        System.out.println("IP Address:- " + inetAddress.getHostAddress());
+        System.out.println("Host Name:- " + inetAddress.getHostName());
+    }
+}
