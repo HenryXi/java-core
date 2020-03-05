a9325866be29d82f3b045506ee4e0d803b2fb0ae
yong <xixiaoyong@xiaomi.com>
2020-03-05 16:48:44 +0800

    boxing test

--- java-core-lang/src/main/java/com/henryxi/java/core/lang/boxing/BoxingClient.java
+++ java-core-lang/src/main/java/com/henryxi/java/core/lang/boxing/BoxingClient.java
@@ -4,6 +4,9 @@ public class BoxingClient {
     public static void main(String[] args) {
         Long value = getLong();
         System.out.println(value);
+        Boolean isNull = null;
+        boolean booleanValue = isNull;
+        System.out.println(booleanValue);
     }
 
     private static long getLong() {
