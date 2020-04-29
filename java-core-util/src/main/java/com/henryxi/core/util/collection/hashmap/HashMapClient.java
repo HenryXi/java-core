fe81b5d40ed3f06bbd5e0642dffa5794c26fe6a7
yong <xixiaoyong@xiaomi.com>
2020-04-29 21:58:44 +0800

    this is auto commit message

--- java-core-util/src/main/java/com/henryxi/core/util/collection/hashmap/HashMapClient.java
+++ java-core-util/src/main/java/com/henryxi/core/util/collection/hashmap/HashMapClient.java
@@ -1,9 +1,14 @@
 package com.henryxi.core.util.collection.hashmap;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HashMapClient {
     public static void main(String[] args) {
-        int hash = 17;
-        int length = 16;
-        System.out.println((length - 1) & hash);
+        Map<String, Integer> map = new HashMap<>();
+        map.put("1", 1);
+        int i = map.get("1");
+        i++;
+        System.out.println(map.get("1"));
     }
 }
